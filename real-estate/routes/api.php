13805c40e21621cf96383219f802cdf571ac1dc0
kuranchieracheal@gmail.com <kuranchieracheal@gmail.com>
2022-12-22 11:59:33 +0000

    routes

--- real-estate/routes/api.php
+++ real-estate/routes/api.php
@@ -22,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('estates', EstateController::class);
 
 Route::get('estate/filter', [EstateController::class, 'filterResults']);
+
+
+
